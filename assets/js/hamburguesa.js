c7cbe4481732626db12eb33536fa8fae5a296337
Matias <matiasbtvs@hotmail.com>
2023-03-24 23:32:28 -0300

    resposive terminado

--- assets/js/hamburguesa.js
+++ assets/js/hamburguesa.js
@@ -1,16 +1,33 @@
 // -------------- MENU HAMBURGUESA ----------------------
 
-document.querySelector(".bars__menu").addEventListener("click", animateBars);
+document.querySelector("#bars__menu").addEventListener("click", animateBars);
+
+document.getElementById("back__menu").addEventListener("click", ocultarMenu )
 
 var line1__bars = document.querySelector(".line1__bars-menu");
 var line2__bars = document.querySelector(".line2__bars-menu");
 var line3__bars = document.querySelector(".line3__bars-menu");
+var nav = document.getElementById("nav")
+var backMenu = document.getElementById("back__menu")
 
 function animateBars() {
     line1__bars.classList.toggle("activeline1__bars-menu")
     line2__bars.classList.toggle("activeline2__bars-menu")
     line3__bars.classList.toggle("activeline3__bars-menu")
+    nav.style.right = "0px";
+    backMenu.style.display = "block"
+
 }
+
+function ocultarMenu (){
+
+    nav.style.right = "-300px";
+    backMenu.style.display = "none"
+
+
+}
+
+
 
 
 
